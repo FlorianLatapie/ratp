5e745704e43b632393fa47e1730dbc92337fe48e
Florian Latapie <70631622+FlorianLatapie@users.noreply.github.com>
2025-08-09 14:35:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -158,18 +158,7 @@ function getDistance(lat1, lon1, lat2, lon2) {
 }
 
 function getDistanceInMeters(lat1, lon1, lat2, lon2) {
-    // Convert to radians in one go
-    const φ1 = lat1 * DEG_TO_RAD;
-    const φ2 = lat2 * DEG_TO_RAD;
-    const Δφ = (lat2 - lat1) * DEG_TO_RAD;
-    const Δλ = (lon2 - lon1) * DEG_TO_RAD;
-    
-    // Calculate half-chord distances
-    const sinΔφ2 = Math.sin(Δφ * 0.5);
-    const sinΔλ2 = Math.sin(Δλ * 0.5);
-    const a = sinΔφ2 * sinΔφ2 + Math.cos(φ1) * Math.cos(φ2) * sinΔλ2 * sinΔλ2;
-    const res = R_KM * 2 * Math.asin(Math.sqrt(a)) * 1000; // Convert to meters
-    return res;
+    return getDistance(lat1, lon1, lat2, lon2) * 1000; // Convert kilometers to meters
 }
 
 async function getStationsAndLinesFromLocation(lat, lon) {
